5251e5eda741ae97dc6f5fb5329cf96dddfc7485
zhengli233 <zhengli2331027@gmail.com>
2018-03-20 23:52:13 -0700

    built the cnn model, but my laptop kills the process when I run the code. don't know why

--- keras_data.py
+++ keras_data.py
@@ -1,5 +1,48 @@
 from keras.preprocessing.image import ImageDataGenerator, load_img, array_to_img, img_to_array
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Conv2DTranspose
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
 import cv2
+
+class FixedImageDataGenerator(ImageDataGenerator):
+    def standardize(self, x):
+        if self.featurewise_center:
+            x = ((x/255.) - 0.5) * 2.
+        return x
+
+img_width, img_height = 256, 256
+
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2DTranspose(128, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+'''
+model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+'''
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
 
 data_gen_args = dict(featurewise_center=True,
                      featurewise_std_normalization=True,
@@ -7,8 +50,8 @@ data_gen_args = dict(featurewise_center=True,
                      width_shift_range=0.1,
                      height_shift_range=0.1,
                      zoom_range=0.2)
-image_datagen = ImageDataGenerator(**data_gen_args)
-mask_datagen = ImageDataGenerator(**data_gen_args)
+image_datagen = FixedImageDataGenerator(**data_gen_args)
+mask_datagen = FixedImageDataGenerator(**data_gen_args)
 
 # Provide the same seed and keyword arguments to the fit and flow methods
 seed = 1
@@ -34,9 +77,10 @@ mask_generator = mask_datagen.flow_from_directory(
 train_generator = zip(image_generator, mask_generator)
 
 
-'''
+
 model.fit_generator(
     train_generator,
     steps_per_epoch=2000,
     epochs=50)
-'''
+
+model.save_weights('first_try.h5')
